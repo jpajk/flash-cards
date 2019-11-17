887f96a44d590bc8861ac58febed64ae8295e297
Jacek <jacekpajak@yandex.com>
2019-11-17 19:25:19 +0100

    Init.

---
+++ App.js
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <View>
+          <Text>Hello world!</Text>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
